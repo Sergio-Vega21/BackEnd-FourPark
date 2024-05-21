e3d378c08d2fd7a51dc372d179d7a5f6e2dfe40b
Sergio-Vega21 <savegac@udistrital.edu.co>
2024-05-21 17:14:58 -0500

    Se agrega endpoints y rutas al tipo de usuario

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const facturaRoutes = require('./src/routes/facturaRoutes');
 const parqueaderoRoutes = require('./src/routes/parqueaderoRoutes'); 
+const rolRoutes = require('./src/routes/rolRoutes'); 
+
 
 const app = express();
 
@@ -11,6 +13,8 @@ app.use(bodyParser.json());
 // Rutas
 app.use('/api', facturaRoutes);
 app.use('/api', parqueaderoRoutes);
+app.use('/api', rolRoutes);
+
 
 // Puerto
 const PORT = process.env.PORT || 3000;
