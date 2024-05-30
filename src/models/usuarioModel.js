e5515a054410ca2428c8c954a64c8612a1ba5b4b
Valentina <valentivelasquezm@gmail.com>
2024-05-30 18:32:55 -0500

    fixfixifixiifi help

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -6,22 +6,27 @@ const emailService = require("../config/email");
 
 module.exports = {
   async create(usuario) {
-    const { correo_electronico, id_rol, nombre, apellido } = usuario;
-    const contrasena = v4();
-    console.log(contrasena);
-    emailService.sendEmail({
-      from: "valentivelasquezm@gmail.com", // Dirección del remitente
-      to: correo_electronico,
-      subject: "Asignación de contraseña",
-      text:
-        "Se le asigno la siguiente contraseña en la plataforma: " + contrasena,
-    });
-    const hash = md5(contrasena, "hex").toString();
-    const result = await pool.query(
-      "INSERT INTO usuario ( correo_electronico, id_rol, nombre, apellido, contrasena) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-      [correo_electronico, id_rol, nombre, apellido, hash]
-    );
-    return result.rows[0];
+    try {
+      const { correo_electronico, id_rol, nombre, apellido } = usuario;
+      const contrasena = v4();
+      emailService.sendEmail({
+        from: "valentivelasquezm@gmail.com", // Dirección del remitente
+        to: correo_electronico,
+        subject: "Asignación de contraseña",
+        text:
+          "Se le asigno la siguiente contraseña en la plataforma: " +
+          contrasena,
+      });
+      const hash = md5(contrasena, "hex").toString();
+      const result = await pool.query(
+        "INSERT INTO usuario ( correo_electronico, id_rol, nombre, apellido, contrasena) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+        [correo_electronico, id_rol, nombre, apellido, hash]
+      );
+      return result.rows[0];
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+    }
   },
   async findAll() {
     const result = await pool.query("SELECT * FROM usuario");
