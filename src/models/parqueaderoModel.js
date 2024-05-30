f590bdc6eb32e77ce8fa90af4073f5bb9591a820
Valentina <valentivelasquezm@gmail.com>
2024-05-30 11:43:18 -0500

    configurado endpoint parqueaderos para mandarlos al front

--- src/models/parqueaderoModel.js
+++ src/models/parqueaderoModel.js
@@ -1,36 +1,96 @@
-const pool = require('../config/database');
+const pool = require("../config/database");
 
 module.exports = {
-    async create(parqueadero) {
-        const { id_parqueadero, idciudad, id_disponibilidad, id_tipo, nombre, cantidad_espacios, precio_minuto_moto, precio_minuto_auto, tarifap_moto, tarifap_auto} = parqueadero;
-        const result = await pool.query(
-            'INSERT INTO parqueadero (id_parqueadero, idciudad, id_disponibilidad, id_tipo, nombre, cantidad_espacios, precio_minuto_moto, precio_minuto_auto, tarifap_moto, tarifap_auto) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9 ,$10) RETURNING *',
-            [id_parqueadero, idciudad, id_disponibilidad, id_tipo, nombre, cantidad_espacios, precio_minuto_moto, precio_minuto_auto, tarifap_moto, tarifap_auto]
-        );
-        return result.rows[0];
-    },
+  async create(parqueadero) {
+    const {
+      id_parqueadero,
+      idciudad,
+      id_disponibilidad,
+      id_tipo,
+      nombre,
+      cantidad_espacios,
+      precio_minuto_moto,
+      precio_minuto_auto,
+      tarifap_moto,
+      tarifap_auto,
+    } = parqueadero;
+    const result = await pool.query(
+      "INSERT INTO parqueadero (id_parqueadero, idciudad, id_disponibilidad, id_tipo, nombre, cantidad_espacios, precio_minuto_moto, precio_minuto_auto, tarifap_moto, tarifap_auto) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9 ,$10) RETURNING *",
+      [
+        id_parqueadero,
+        idciudad,
+        id_disponibilidad,
+        id_tipo,
+        nombre,
+        cantidad_espacios,
+        precio_minuto_moto,
+        precio_minuto_auto,
+        tarifap_moto,
+        tarifap_auto,
+      ]
+    );
+    return result.rows[0];
+  },
 
-    async findAll() {
-        const result = await pool.query('SELECT * FROM parqueadero');
-        return result.rows;
-    },
+  async findAll() {
+    const result = await pool.query(`
+    SELECT p.*, u.direccion, d.hora_apertura, d.hora_cierre
+    FROM parqueadero p
+    JOIN ubicacion u ON p.idciudad = u.idciudad
+    JOIN disponibilidad d ON p.id_disponibilidad = d.id_disponibilidad;
 
-    async findById(id) {
-        const result = await pool.query('SELECT * FROM parqueadero WHERE id_parqueadero = $1', [id]);
-        return result.rows[0];
-    },
+    `);
+    return result.rows;
+  },
 
-    async update(id, parqueadero) {
-        const { idciudad, id_disponibilidad, id_tipo, nombre, cantidad_espacios, precio_minuto_moto, precio_minuto_auto, tarifap_moto, tarifap_auto} = parqueadero;
-        const result = await pool.query(
-            'UPDATE parqueadero SET idciudad = $2, id_disponibilidad = $3, id_tipo = $4, nombre = $5, cantidad_espacios = $6, precio_minuto_moto = $7, precio_minuto_auto = $8, tarifap_moto = $9, tarifap_auto = $10 WHERE id_parqueadero = $1 RETURNING *',
-            [id, idciudad, id_disponibilidad, id_tipo, nombre, cantidad_espacios, precio_minuto_moto, precio_minuto_auto, tarifap_moto, tarifap_auto]
-        );
-        return result.rows[0];
-    },
+  async findById(id) {
+    const result = await pool.query(
+      `
+      SELECT p.*, u.direccion
+      FROM parqueadero p
+      JOIN ubicacion u ON p.idciudad = u.idciudad
+      WHERE p.id_parqueadero = $1
+    `,
+      [id]
+    );
+    return result.rows[0];
+  },
 
-    async delete(id) {
-        const result = await pool.query('DELETE FROM parqueadero WHERE id_parqueadero = $1 RETURNING *', [id]);
-        return result.rows[0];
-    }
+  async update(id, parqueadero) {
+    const {
+      idciudad,
+      id_disponibilidad,
+      id_tipo,
+      nombre,
+      cantidad_espacios,
+      precio_minuto_moto,
+      precio_minuto_auto,
+      tarifap_moto,
+      tarifap_auto,
+    } = parqueadero;
+    const result = await pool.query(
+      "UPDATE parqueadero SET idciudad = $2, id_disponibilidad = $3, id_tipo = $4, nombre = $5, cantidad_espacios = $6, precio_minuto_moto = $7, precio_minuto_auto = $8, tarifap_moto = $9, tarifap_auto = $10 WHERE id_parqueadero = $1 RETURNING *",
+      [
+        id,
+        idciudad,
+        id_disponibilidad,
+        id_tipo,
+        nombre,
+        cantidad_espacios,
+        precio_minuto_moto,
+        precio_minuto_auto,
+        tarifap_moto,
+        tarifap_auto,
+      ]
+    );
+    return result.rows[0];
+  },
+
+  async delete(id) {
+    const result = await pool.query(
+      "DELETE FROM parqueadero WHERE id_parqueadero = $1 RETURNING *",
+      [id]
+    );
+    return result.rows[0];
+  },
 };
