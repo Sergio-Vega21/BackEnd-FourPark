842e4bf71d3bf263f258b19da2745af253f8440e
Valentina <valentivelasquezm@gmail.com>
2024-05-30 16:12:37 -0500

    WIP on main

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,19 +1,22 @@
-const pool = require('../config/database');
+const pool = require("../config/database");
+
+// Métodos existentes
 
 module.exports = {
-    async create(usuario) {
-        const { correo_electronico, id_rol, nombre, apellido, contrasena} = usuario;
-        const result = await pool.query(
-            'INSERT INTO usuario ( correo_electronico, id_rol, nombre, apellido, contrasena) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-            [ correo_electronico, id_rol, nombre, apellido, contrasena]
-        );
-        return result.rows[0];
-    },
+  async create(usuario) {
+    const { correo_electronico, id_rol, nombre, apellido, contrasena } =
+      usuario;
+    const result = await pool.query(
+      "INSERT INTO usuario ( correo_electronico, id_rol, nombre, apellido, contrasena) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [correo_electronico, id_rol, nombre, apellido, contrasena]
+    );
+    return result.rows[0];
+  },
 
-    async findAll() {
-        const result = await pool.query('SELECT * FROM usuario');
-        return result.rows;
-    },
+  async findAll() {
+    const result = await pool.query("SELECT * FROM usuario");
+    return result.rows;
+  },
 
     async findById(usuario) {
         const {correo_electronico} = usuario;
@@ -21,18 +24,14 @@ module.exports = {
         return result.rows[0];
     },
 
-    async update(id, usuario) {
-        const { id_rol, nombre, apellido, contrasena} = usuario;
-        const result = await pool.query(
-            'UPDATE usuario SET id_rol = $2, nombre= $3, apellido = $4, contrasena = $5  WHERE correo_electronico= $1 RETURNING *',
-            [id, id_rol, nombre, apellido, contrasena]
-        );
-        return result.rows[0];
-    },
+  async update(id, usuario) {
+    const { id_rol, nombre, apellido, contrasena } = usuario;
+    const result = await pool.query(
+      "UPDATE usuario SET id_rol = $2, nombre= $3, apellido = $4, contrasena = $5  WHERE correo_electronico= $1 RETURNING *",
+      [id, id_rol, nombre, apellido, contrasena]
+    );
+    return result.rows[0];
+  },
 
-    async delete(id) {
         const {correo_electronico} = id;
-        const result = await pool.query('DELETE FROM usuario WHERE correo_electronico  = $1 RETURNING *', [correo_electronico]);
-        return result.rows[0];
-    }
 };
