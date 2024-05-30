9db97595b19e24692fa3c277e13f772e7f287988
lopezander <ander.jeffrey@gmail.com>
2024-05-30 17:39:46 -0500

    se agrega reporte y rutas a app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const usuarioRoutes = require("./src/routes/usuarioRoutes");
 const cors = require("cors");
 const tarjetaRoutes = require("./src/routes/tarjetaRoutes");
 const loginRoutes = require("./src/routes/loginRoutes");
+const path = require('path');
 
 const app = express(); // Inicializamos express aquí
 
@@ -21,6 +22,14 @@ app.use("/api", rolRoutes);
 app.use("/api", usuarioRoutes);
 app.use("/api", tarjetaRoutes);
 app.use("/api", loginRoutes);
+app.use('/reports', reportRoutes);
+
+app.use(express.static(path.join(__dirname, '../frontend-repo/public')));
+// Usar las rutas de reportes
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
 
 // Puerto
 const PORT = process.env.PORT || 3000;
